c6181ea124cdc20500b7e7696b53d994fb2004e3
Azkeradalaz <daazwaa@gmail.com>
2024-09-30 14:35:02 +0300

    Project created

---
+++ src/test/java/ru/baldursgate3/tgbot/storeservice/StoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.baldursgate3.tgbot.storeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
