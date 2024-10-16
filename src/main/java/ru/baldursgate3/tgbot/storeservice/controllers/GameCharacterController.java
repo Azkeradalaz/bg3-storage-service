3e2e23c3c6bd1516ba598925558f56a78c168077
Azkeradalaz <daazwaa@gmail.com>
2024-10-16 11:27:03 +0300

    поправил RequestMappinf

--- src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/GameCharacterController.java
+++ src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/GameCharacterController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/gamecharacter")
+@RequestMapping("/character")
 public class GameCharacterController {
 
     private final GameCharacterService gameCharacterService;
