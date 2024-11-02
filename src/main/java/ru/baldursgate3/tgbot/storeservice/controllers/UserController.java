a58486702c91bbc94f3442560828d6275e71759a
Azkeradalaz <daazwaa@gmail.com>
2024-11-02 19:58:28 +0300

    dto переделка

--- src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/UserController.java
+++ src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.baldursgate3.tgbot.storeservice.entities.User;
+import ru.baldursgate3.tgbot.storeservice.models.UserDto;
 import ru.baldursgate3.tgbot.storeservice.services.UserService;
 
 
@@ -16,20 +17,24 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    ResponseEntity<String> create(@RequestBody User user){
-        User newUser = userService.create(user);
+    ResponseEntity<String> create(@RequestBody UserDto user){
+        User newUser = new User();
+        newUser.setName(user.name());
+        newUser.setTgUserId(user.tgUserId());
+        userService.create(newUser);
         return new ResponseEntity<>(newUser.getName(),HttpStatus.CREATED);
     }
 
     @GetMapping("/tgid/{tgId}")
-    ResponseEntity<User> findByTgId(@PathVariable Long tgId){
+    ResponseEntity<UserDto> findByTgId(@PathVariable Long tgId){
         User user = null;
         user = userService.findByTgId(tgId);
+        UserDto userDto = new UserDto(user.getName(), user.getTgUserId());
         if(user == null){
             return new ResponseEntity<>(null,HttpStatus.OK);
         }
         else {
-            return new ResponseEntity<>(user,HttpStatus.OK);
+            return new ResponseEntity<>(userDto,HttpStatus.OK);
         }
 
     }
