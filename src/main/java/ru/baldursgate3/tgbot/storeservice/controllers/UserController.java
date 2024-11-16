40b3f05a5f626bfb9de7fc4e500699d5ce3dac42
Azkeradalaz <daazwaa@gmail.com>
2024-11-16 13:06:14 +0300

    исправление багов регистрации

--- src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/UserController.java
+++ src/main/java/ru/baldursgate3/tgbot/storeservice/controllers/UserController.java
@@ -19,7 +19,11 @@ public class UserController {
     @PostMapping
     ResponseEntity<String> create(@RequestBody UserDto user){
         User newUser = new User();
-        newUser.setName(user.name());
+        String userName = user.name();
+        if(userName.length()>30){
+            userName = userName.substring(0,30);
+        }
+        newUser.setName(userName);
         newUser.setTgUserId(user.tgUserId());
         userService.create(newUser);
         return new ResponseEntity<>(newUser.getName(),HttpStatus.CREATED);
@@ -28,12 +32,11 @@ public class UserController {
     @GetMapping("/tgid/{tgId}")
     ResponseEntity<UserDto> findByTgId(@PathVariable Long tgId){
         User user = userService.findByTgId(tgId);
-        UserDto userDto = new UserDto(user.getName(), user.getTgUserId());
         if(user == null){
-            return new ResponseEntity<>(null,HttpStatus.OK);
-        }
-        else {
-            return new ResponseEntity<>(userDto,HttpStatus.OK);
+            return new ResponseEntity<>(null,HttpStatus.OK);//так и задумывалось
+        }else {
+            UserDto userDto = new UserDto(user.getName(), user.getTgUserId());
+            return new ResponseEntity<>(userDto, HttpStatus.OK);
         }
 
     }
